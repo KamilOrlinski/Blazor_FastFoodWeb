60b53ef45788834f6e50109f211ca7bad5c60f87
KamilOrlinski <kamilorlik17@gmail.com>
2025-01-17 15:03:12 +0100

    before migration

--- Models/OrderItems.cs
+++ Models/OrderItems.cs
@@ -4,12 +4,14 @@ namespace FastFoodWeb.Models
 {
     public class Category
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public List<OrderItems> Items { get; set; }
 
     }
     public class OrderItems
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
